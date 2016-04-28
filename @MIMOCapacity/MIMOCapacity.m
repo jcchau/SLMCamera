149da2f47d1df6f508bd312162480876c6ef1a05
Jimmy C. Chau <jchau@bu.edu>
2016-04-28 04:37:30 -0400

    Moved old methods that incorrectly assume that a uniformly-distributed x maximizes the capacity of y=G*x+w from class MIMOCapacity to new class MIMOCapacityOld.
    This is a clean-up to get them out of the way.

--- @MIMOCapacity/MIMOCapacity.m
+++ @MIMOCapacity/MIMOCapacity.m
@@ -8,11 +8,6 @@ classdef MIMOCapacity
     methods(Static)
         de = calculateDiffEntropyFromPmf(pmf, bin_size)
         
-        [pmf, trials, y_min, y_max, hits] = ...
-            generateReceivedPmfForUniformInput( ...
-            G, x_max, variance_noise_out, bins_per_dimension, ...
-            min_trials, trials_per_batch)
-        
         li = convertToLinearIndex(weights, subs)
         weights = convertToLinearIndexWeights(mat_size)
         msi = convertLinearToSubscriptIndex(weights, li)
@@ -25,13 +20,6 @@ classdef MIMOCapacity
         de = calculateDiffEntropyOfGaussian(variance)
         
         nbins = fillMaxNBins(max_nbins, dimensions)
-        
-        [nats, min_nats, variance_H, nbins, trials, h_y, pmf] = ...
-            calculateCapacityForUniformInput( ...
-            G, x_max, variance_noise_out, max_nbins, min_trials)
-        
-        variance = calculateVarianceOfDiffEntropyFromMonteCarloPmf( ...
-            pmf, num_trials)
         
         [de, pmf] = calculateDiffEntropyOfClippedNormal( ...
             a, b, nbins, mu, sigma)
