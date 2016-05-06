220ed15118455b892626d1b064f1939ec2c43b17
Jimmy C. Chau <jchau@bu.edu>
2016-05-06 14:23:37 -0400

    Added method MIMOCapacity.computeCapacityLBUnifX.

---
+++ @MIMOCapacity/computeCapacityLBUnifX.m
@@ -0,0 +1,16 @@
+function lb_nats = computeCapacityLBUnifX(G, x_max, sigma_w)
+%COMPUTECAPACITYLBUNIFX Summary of this function goes here
+%   Detailed explanation goes here
+
+[n_r, ~] = size(G);
+
+variance_noise_out = repmat(sigma_w^2, n_r, 1);
+
+max_nbins = 1e9/8/5;
+min_trials = 1e9;
+
+[~, lb_nats] = MIMOCapacityLBUnifX.calculateCapacityLBForUniformInput( ...
+    G, x_max, variance_noise_out, max_nbins, min_trials);
+
+end
+
