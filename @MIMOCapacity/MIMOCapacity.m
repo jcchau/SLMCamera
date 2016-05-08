0e68c3a10ee80d2f2c37f331ba7f87ca5abf195a
Jimmy C. Chau <jchau@bu.edu>
2016-05-08 15:18:26 -0400

    Modified MIMOCapacity.computeCapacityLBUnifX to also return the regular mutual information estimate (in addition to the conservative lower bound).

--- @MIMOCapacity/MIMOCapacity.m
+++ @MIMOCapacity/MIMOCapacity.m
@@ -45,7 +45,8 @@ classdef MIMOCapacity
         ub_nats = computeCapacityUBElMoslimany2014(G, x_max, sigma_w)
         ub_nats = computeCapacityUBMaxVariance(G, x_max, sigma_w)
         lb_nats = computeCapacityLBMRC(G, x_max, sigma_w)
-        lb_nats = computeCapacityLBUnifX(G, x_max, sigma_w)
+        [lb_nats_conservative, lb_nats] = ...
+            computeCapacityLBUnifX(G, x_max, sigma_w)
         
         nats = calculateDiffEntropyOfMVGaussian(K)
         
--- @MIMOCapacity/computeCapacityLBUnifX.m
+++ @MIMOCapacity/computeCapacityLBUnifX.m
@@ -1,4 +1,5 @@
-function lb_nats = computeCapacityLBUnifX(G, x_max, sigma_w)
+function [lb_nats_conservative, lb_nats] = ...
+    computeCapacityLBUnifX(G, x_max, sigma_w)
 %COMPUTECAPACITYLBUNIFX Summary of this function goes here
 %   Detailed explanation goes here
 
@@ -9,7 +10,8 @@ variance_noise_out = repmat(sigma_w^2, n_r, 1);
 max_nbins = 1e9/8/5;
 min_trials = 1e9;
 
-[~, lb_nats] = MIMOCapacityLBUnifX.calculateCapacityLBForUniformInput( ...
+[lb_nats, lb_nats_conservative] = ...
+    MIMOCapacityLBUnifX.calculateCapacityLBForUniformInput( ...
     G, x_max, variance_noise_out, max_nbins, min_trials);
 
 end
