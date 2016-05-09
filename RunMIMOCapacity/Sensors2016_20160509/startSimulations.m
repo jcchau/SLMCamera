5dc86a4dd2d26eb8193a368b19395ce6ebf83943
Jimmy C. Chau <jchau@bu.edu>
2016-05-09 19:26:24 -0400

    Added startSimulations.m and runTrial.m to RunMIMOCapacity/Sensors2016_20160509.
    Not yet tested.

---
+++ RunMIMOCapacity/Sensors2016_20160509/startSimulations.m
@@ -0,0 +1,20 @@
+function startSimulations(runTrial_cmd, savedir)
+% runTrial_cmd is the command used to start the next stage of the
+% simulation.  
+
+srand = 20160509;
+rng(srand);
+
+ntrials = 1000;
+srand_trial = randi(intmax, ntrials, 1);
+
+for ii = 1:ntrials
+    matlab_cmd = sprintf('%s %d %d "%s"', ...
+        runTrial_cmd, ii, srand_trial(ii), savedir);
+    job_cmd = sprintf('qsub -N simt%d -cwd -j y -m a -V -b y %s', ...
+        ii, matlab_cmd);
+    system(job_cmd);
+end % for ii
+
+end
+
